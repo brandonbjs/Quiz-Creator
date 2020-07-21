aeef1e8213e130a542aa6a1b8bd996ae84740bc8
StevenSprouls <49702096+StevenSprouls@users.noreply.github.com>
2020-07-21 13:22:10 -0700

    started test taking functions, not complete.

--- Quiz-Creator/TakerScreen.cs
+++ Quiz-Creator/TakerScreen.cs
@@ -12,29 +12,70 @@ namespace Quiz_Creator
 {
     public partial class TakerScreen : Form
     {
+        int currentQuestionIndex;
+
+        Quiz currentlyTakingQuiz;
+
         public TakerScreen()
         {
             InitializeComponent();
         }
 
+        public TakerScreen(string filename)
+        {
+            // Call this contructor with a filename to open a quiz for taking
+            InitializeComponent();
+
+            if (File.Exists(filename))
+            {
+                currentlyTakingQuiz = readQuizFile(filename);
+            }
+            else
+            {
+                MessageBox.Show("The quiz file \"" + filename + "\" does not exist.");
+                this.Close();
+            }
+            for (int i = 0; i < currentlyTakingQuiz.Questions.Count; i++)
+            {
+                listboxQuestions.Items.Add(currentlyTakingQuiz.Questions[i].Prompt);
+            }
+            if (currentlyTakingQuiz.Questions.Count <= 0)
+            {
+                MessageBox.Show("There are no more questions.");
+            }
+        }
+
         private void buttonBack_Click(object sender, EventArgs e)
         {
             // Back Button
+            currentQuestionIndex - 1;
         }
 
         private void buttonNext_Click(object sender, EventArgs e)
         {
             // Next Button
+            currentQuestionIndex + 1;
         }
 
         private void textboxAnswer_TextChanged(object sender, EventArgs e)
         {
             // Answer Text changed
+            currentlyTakingQuiz.Questions[currentQuestionIndex].Answer = textboxAnswerEdit.Text;
         }
 
         private void buttonSubmitQuestion_Click(object sender, EventArgs e)
         {
             // Submit Question (Same as Next, unless we are on the last question and/or all questions are filled out)
+            if (currentQuestionIndex + 1 != null)
+            {
+                currentQuestionIndex + 1;
+            }
+            else
+            {
+                string message = "That was the last Question, the quiz has been saved and submitted.";
+                MessageBox.Show(message);
+                //just save the file for now, can have it graded and submitted at later implementation
+            }
         }
 
         private void buttonSubmitQuiz_Click(object sender, EventArgs e)
@@ -42,6 +83,9 @@ namespace Quiz_Creator
             // Submit Quiz (Submits all questions)
 
             // For implementation 1, show results as "you got x/total" in a popup/messagebox
+            string message = "You got x/total";
+            string title = "Grade";
+            MessageBox.Show(message, title);
 
             // For future implementations, save quiz here
         }
